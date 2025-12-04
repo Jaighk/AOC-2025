4e01ec76911188194f86086741975c0c5d867d0e
Jake Parker <jacob.parker51@gmail.com>
2025-12-03 20:12:20 -0600

    day 3 complete

--- day_3/day_3_part_two.py
+++ day_3/day_3_part_two.py
@@ -1,68 +1,30 @@
 import sys
 
-def main() -> None:
-    args: list[str] = sys.argv
-    mode: str = args[1]
-    file: str = get_file(mode)
-    test_value: int = 3121910778619
+INPUT_FILE = "inputs/input.txt"
 
-    banks: list[str] = [line.strip() for line in open(file=file, mode="r")]
-    total_output_joltage: int = 0
+def getmax(string: str, maxlen: int) -> str: 
+    result = ""
+    win_start, win_stop = 0, len(string) - maxlen + 1
 
-    for bank in banks:
-        max_joltage: int = get_joltage(bank)
-        total_output_joltage += max_joltage
-    
-    print(f"Test val: {test_value}")
-    print(f"Total output joltage: {total_output_joltage}")
-    print(f"Diff = {test_value - total_output_joltage}")
-        
+    while len(result) < maxlen:
+        substring = string[win_start:win_stop]
+        win_start += substring.index(max(substring))
+        if win_start == win_stop - 1:
+            result += string[win_start:]
+            break
+        result += string[win_start]
+        win_start += 1
+        win_stop += 1
+    return result
 
-def get_joltage(bank: str) -> int:
-    batteries_required: int = 12
-    enabled_batteries: str = ""
+def solve(maxlen):
+    total = 0
+    with open(INPUT_FILE, "r") as f:
+        lines = [line.strip() for line in f]
+        for line in lines:
+            number = getmax(line, maxlen)
+            total += int(number)
+    print(total)
 
-    # Find highest battery from bank with enough room for all the batteries
-    first_battery, first_battery_index = get_first_battery(bank, batteries_required=batteries_required)
-    enabled_batteries += first_battery
-    enabled_batteries += get_rest_of_batteries(battery_pool=bank[first_battery_index+1:], batteries_required=batteries_required-1)
-    return int(enabled_batteries)
-
-
-def get_first_battery(bank: str, batteries_required: int) -> tuple[str, int]:
-    first_battery: str = str()
-    first_battery_index: int = int()
-
-    # find the highest battery that still leaves room for the other 11
-    for battery in bank[:-batteries_required]:
-        if not first_battery:
-            first_battery = battery
-            first_battery_index = bank.index(battery)
-            continue
-        if int(battery) > int(first_battery):
-            first_battery = battery
-            first_battery_index = bank.index(battery)
-
-    return first_battery, first_battery_index
-
-
-def get_rest_of_batteries(battery_pool: str, batteries_required: int, ) -> str:
-    remaining_batteries: list[int] = [int(battery) for battery in battery_pool]
-    while len(remaining_batteries) != batteries_required:
-        remaining_batteries.remove(min(remaining_batteries))
-    remaining_batteries = "".join([str(battery) for battery in remaining_batteries])
-    return remaining_batteries
-
-
-def get_file(mode:str) -> str:
-    file: str = str()
-    if not mode:
-        print("Mode not specified")
-    if mode == "test":
-        file: str = "./inputs/test.txt"
-    if mode == "puzzle":
-        file: str = "./inputs/input.txt"
-    return file
-
-if __name__ == "__main__":
-    main()
+solve(2)
+solve(12)
