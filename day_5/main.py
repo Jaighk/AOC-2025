a2c082ee12f40874da71a08e749b4c8f726d6624
Jacob Parker <jacob.parker51@gmail.com>
2025-12-06 15:35:13 -0600

    started day 5 part 2

--- day_5/main.py
+++ day_5/main.py
@@ -1,11 +1,12 @@
+from re import A
 import sys
 
 def main() -> None:
     input_file_path: str = get_input_file(mode = sys.argv[1] if len(sys.argv) > 1 else "")
     input: list[str] = [line for line in open(file=input_file_path, mode="r")]
     ranges, ingredients = parse_input(input)
-
     print(f"Fresh: {count_fresh(ranges, ingredients)}")
+    print(f"Total Fresh IDs: {count_fresh_ids(ranges)}")
 
 
 def parse_input(input: list[str]) -> tuple[list[str], list[str]]:
@@ -25,7 +26,6 @@ def count_fresh(ranges: list[str], ingredients: list[str]) -> int:
     count: int = 0
     found_fresh: list[str] = []
     for ingredient in ingredients:
-        print(f"Checking {ingredient}")
         for id_range in ranges:
             if ingredient in found_fresh: 
                 continue
@@ -33,9 +33,19 @@ def count_fresh(ranges: list[str], ingredients: list[str]) -> int:
             range_max: int = int(id_range.split(sep="-")[1])
             if int(ingredient) in range(range_min, range_max + 1):
                 found_fresh.append(ingredient)
-                print(f"ID: {ingredient} in range {id_range}")
                 count += 1
     return count
+
+
+def count_fresh_ids(ranges: list[str]) -> int:
+    counted: list[int] = []
+    for id_range in ranges: 
+        range_min: int = int(id_range.split(sep="-")[0])
+        range_max: int = int(id_range.split(sep="-")[1])
+        for id in range(range_min, range_max + 1):
+            if id not in counted:
+                counted.append(id)
+    return len(counted)
 
 
 def get_input_file(mode: str) -> str:
