af0a4ab3fa059b23b540de766903e919842284d2
jaighk <jacob.parker51@gmail.com>
2025-12-09 13:14:59 -0600

    day 7 complete :

--- day_7/main.py
+++ day_7/main.py
@@ -1,58 +1,41 @@
 import sys
+from collections import defaultdict
+
 
 START: str = "S"
 SPACE: str = "."
-SPLIT: str = "^"
+SPLITTER: str = "^"
 BEAM: str = "|"
-SPLIT_COUNT: int = 0
-BEAM_LOCATIONS: list[int] = []
+split_count: int = 0
+beam_locations: list[int] = []
 
 
 def main() -> None:
+    global split_count
     input_file_path: str = get_input_file(mode=sys.argv[1] if len(sys.argv) > 1 else "")
     input: list[str] | list[list[str]] = [
         line for line in open(file=input_file_path, mode="r")
     ]
     for string in range(len(input)):
         input[string] = list(input[string].strip())
+    
+    beams = {input[0].index("S"): 1}
 
-    run_manifold(input)
-    print("----- Part One -----")
-    print(f"Split count: {SPLIT_COUNT}")
+    for i in range(1, len(input)): 
+        line = input[i]
+        new_beams = defaultdict(int)
+        for k, v in beams.items():
+            if line[k] == SPACE:
+                new_beams[k] += v
+            else:
+                split_count += 1
+                new_beams[k - 1] += v
+                new_beams[k + 1] += v
+        beams = new_beams
 
+    print(f"{split_count = }")
+    print(f"{sum(beams.values())}")
 
-def run_manifold(input: list[list[str]]) -> None:
-    global SPLIT_COUNT
-    print("\n")
-    for i in range(len(input)):
-        print(BEAM_LOCATIONS)
-        if i == 0:
-            start: int = input[i].index("S")
-            BEAM_LOCATIONS.append(start)
-            continue
-        for location in BEAM_LOCATIONS:
-            if input[i][location] == SPACE:
-                input[i][location] = BEAM
-            if input[i][location] == SPLIT:
-                print(f"splitting at location: {location}")
-                BEAM_LOCATIONS.remove(location)
-                if location not in (0, len(input[i])):
-                    possible_locations: list[int] = [location - 1, location + 1]
-                if location == 0:
-                    possible_locations = [location + 1]
-                if location == len(input[i]):
-                    possible_locations = [location - 1]
-                while possible_locations:
-                    print(f"{possible_locations = }")
-                    new_beam: int = possible_locations.pop()
-                    if input[i][new_beam] == SPACE:
-                        print(f"new beams at: {new_beam}")
-                        input[i][new_beam] = BEAM
-                        BEAM_LOCATIONS.append(new_beam)
-                SPLIT_COUNT += 1
-
-    for i in input:
-        print("".join(i))
 
 
 def get_input_file(mode: str) -> str:
